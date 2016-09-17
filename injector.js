071a8f202db9a0fd2ed76e84fdda8748b1408731
xiad <xia.ldj@gmail.com>
2016-09-17 23:51:58 +0800

    first step

--- injector.js
+++ injector.js
@@ -1,25 +1,13 @@
+/**
+ * please implement: inject, get
+*/
 function createInjector() {
   var cache = {};
 
   return {
-    inject: inject,
-    get: get
+    inject: null,
+    get: null
   };
-
-  function get(name) {
-    return cache[name];
-  }
-
-  function inject(name) {
-    if (array.isarray(sth)) {
-      var args = sth.slice(0, -1).map(item => get(item));
-      cache[name] = sth[sth.length - 1].apply(null, args);
-    } else if (typeof sth === 'function') {
-      cache[name] = sth();
-    } else {
-      cache[name] = sth;
-    }
-  }
 
 }
 
--- test/test.js
+++ test/test.js
@@ -19,98 +19,18 @@ describe('Test naive dependency injection', function() {
   });
 
   it('should inject by factory function', function() {
-    injector.inject('car', function() {
-      return {
-        run: function() {
-          return 'running';
-        },
-        brake: function() {
-          return 'braking';
-        }
-      }
-    });
-    var car = injector.get('car');
-    assert.equal(car.run(), 'running');
   });
 
   it('should inject dependencies in array', function() {
-    injector.inject('car', ['wheel', 'engine', function(wheel, engine) {
-      return {
-        description: function() {
-          return 'bmw';
-        }
-      }
-    }]);
-
-    var car = injector.get('car');
-    car.should.be.a('object');
-
   });
 
   it('should inject dependencies', function() {
- 
-    injector.inject('wheel', {
-      count: 4
-    });
-
-    injector.inject('engine', {
-      brand: 'oooo'
-    });
-
-    injector.inject('car', ['wheel', 'engine', function(wheel, engine) {
-      return {
-        description: function() {
-          return wheel.count + ' wheels, ' + engine.brand + ' engine';
-        }
-      }
-    }]);
-
-    var car = injector.get('car');
-
-    console.log(car.description());
-    car.description().should.be.a('string');
-
-   
   });
 
   it('should inject by reflection', function() {
-
-    injector.inject('wheel', {
-      count: 4
-    });
-
-    injector.inject('engine', {
-      brand: 'oooo'
-    });
-
-    injector.inject('car', function(wheel, engine) {
-      return {
-        description: function() {
-          return wheel.count + ' wheels, ' + engine.brand + ' engine';
-        }
-      }
-    });
-
   });
 
   it('should inject by provider', function() {
- 
-    injector.inject('car', ['wheel', 'engine', function(wheel, engine) {
-      return {
-        description: function() {
-          return wheel.count + ' wheels, ' + engine.brand + ' engine';
-        }
-      }
-    }]);
-
-    injector.inject('wheel', {
-      count: 4
-    });
-
-    injector.inject('engine', {
-      brand: 'oooo'
-    });
-
   });
 
 });
